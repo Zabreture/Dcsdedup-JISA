a8700a513e5a64213ffaedc739e2b01b5288165b
Zabreture <bo.zhang@mail.nwpu.edu.cn>
2023-03-18 14:40:25 +0800

    Update

--- workload/Figure.py
+++ workload/Figure.py
@@ -1,4 +1,4 @@
-#%%
+# %%
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
@@ -7,114 +7,124 @@ from matplotlib import rcParams
 
 storeData = pd.read_csv('./store.csv')
 retData = pd.read_csv('./retrieve.csv')
-factor = 0.95
+factor = 0.77
 
-
-#%%
+# %%
 length = round(retData['mleIU'].size)
 xLabels = [
-    '2 MB','4 MB','6 MB','8 MB','10 MB'
+    '2 MB', '4 MB', '6 MB', '8 MB', '10 MB'
 ]
 config = {
-    "font.family":'Times New Roman',
+    "font.family": 'Times New Roman',
     "font.size": 18,
-    "mathtext.fontset":'stix',
+    "mathtext.fontset": 'stix',
     "font.serif": ['Times New Roman'],
 }
 rcParams.update(config)
-plt.figure(1,[8.5,6])
-plt.ylabel('Time cost (ms)',fontsize=25)
-plt.xlabel('File size',fontsize=25)
+plt.figure(1, [8.5*factor, 6*factor])
+plt.ylabel('Time cost (ms)', fontsize=25)
+plt.xlabel('File size', fontsize=25)
 plt.xticks(range(length), xLabels)
-plt.grid(zorder = -1, axis='y')
+plt.grid(zorder=-1, axis='y')
 
-barWidth = 0.18
+barWidth = 0.15
 bars1 = storeData['mleIU']
 bars2 = storeData['hurIU']
-bars3 = storeData['enhancedIU']
-bars4 = storeData['dynamicIU']
+bars3 = storeData['tianIU']
+bars4 = storeData['enhancedIU']
+bars5 = storeData['dynamicIU']
 
-r1 = np.arange(len(bars1)) - barWidth*1.5
+r1 = np.arange(len(bars1)) - barWidth * 2
 r2 = [x + barWidth for x in r1]
 r3 = [x + barWidth for x in r2]
 r4 = [x + barWidth for x in r3]
+r5 = [x + barWidth for x in r4]
 
-plt.bar(r1, bars1, width=barWidth,color='#003a8c',edgecolor='#002766',zorder=100, label='CE')
-plt.bar(r2, bars2, width=barWidth,color='#096dd9',edgecolor='#002766',zorder=100, label='Hur et al.')
-plt.bar(r3, bars3, width=barWidth,color='#40a9ff',edgecolor='#002766',zorder=100, label='Enhanced')
-plt.bar(r4, bars4, width=barWidth,color='#bae7ff',edgecolor='#002766',zorder=100, label='Dynamic')
+plt.bar(r1, bars1, width=barWidth, color='#001d66', edgecolor='#002766', zorder=100, label='CE')
+plt.bar(r2, bars2, width=barWidth, color='#003eb3', edgecolor='#002766', zorder=100, label='Hur et al.')
+plt.bar(r3, bars3, width=barWidth, color='#1677ff', edgecolor='#002766', zorder=100, label='Tian et al.')
+plt.bar(r4, bars4, width=barWidth, color='#69b1ff', edgecolor='#002766', zorder=100, label='Enhanced')
+plt.bar(r5, bars5, width=barWidth, color='#bae0ff', edgecolor='#002766', zorder=100, label='Dynamic')
 
 plt.legend(
-    ['CE','Hur et al.','Enhanced','Dynamic'],
+    ['CE', 'Hur et al.', 'Tian et al.', 'Enhanced', 'Dynamic'],
     fontsize=18,
     handlelength=1,
-    ncol=4,
+    ncol=1,
     loc='upper left',
 )
-plt.ylim([0,120])
+# plt.ylim([0, 80])
 plt.tight_layout()
-plt.savefig(r'D:\NutStore\BoAndHarry\Paper\2022 JISA\Paper JISA 2022\figures\IUstore.pdf',bbox_inches='tight')
+plt.savefig(r'D:\NutStore\BoAndHarry\Paper\2023 JISA\2nd-submission\paper\figures\Fig. 4-a-IU-upload.pdf', bbox_inches='tight')
 plt.show()
 
-#%%
-plt.figure(2,[8.5,6])
-plt.ylabel('Time cost (ms)',fontsize=25)
-plt.xlabel('File size',fontsize=25)
+# %%
+plt.figure(2, [8.5*factor, 6*factor])
+plt.ylabel('Time cost (ms)', fontsize=25)
+plt.xlabel('File size', fontsize=25)
 plt.xticks(range(length), xLabels)
-plt.grid(zorder = -1, axis='y')
+plt.grid(zorder=-1, axis='y')
 
-barWidth = 0.18
+barWidth = 0.15
 bars1 = storeData['mleSU']
 bars2 = storeData['hurSU']
-bars3 = storeData['enhancedSU']
-bars4 = storeData['dynamicSU']
+bars3 = storeData['tianSU']
+bars4 = storeData['enhancedSU']
+bars5 = storeData['dynamicSU']
 
-r1 = np.arange(len(bars1)) - barWidth*1.5
+r1 = np.arange(len(bars1)) - barWidth * 2
 r2 = [x + barWidth for x in r1]
 r3 = [x + barWidth for x in r2]
 r4 = [x + barWidth for x in r3]
+r5 = [x + barWidth for x in r4]
 
-plt.bar(r1, bars1, width=barWidth,color='#003a8c',edgecolor='#002766',zorder=100, label='CE')
-plt.bar(r2, bars2, width=barWidth,color='#096dd9',edgecolor='#002766',zorder=100, label='Hur et al.')
-plt.bar(r3, bars3, width=barWidth,color='#40a9ff',edgecolor='#002766',zorder=100, label='Enhanced')
-plt.bar(r4, bars4, width=barWidth,color='#bae7ff',edgecolor='#002766',zorder=100, label='Dynamic')
+plt.bar(r1, bars1, width=barWidth, color='#001d66', edgecolor='#002766', zorder=100, label='CE')
+plt.bar(r2, bars2, width=barWidth, color='#003eb3', edgecolor='#002766', zorder=100, label='Hur et al.')
+plt.bar(r3, bars3, width=barWidth, color='#1677ff', edgecolor='#002766', zorder=100, label='Tian et al.')
+plt.bar(r4, bars4, width=barWidth, color='#69b1ff', edgecolor='#002766', zorder=100, label='Enhanced')
+plt.bar(r5, bars5, width=barWidth, color='#bae0ff', edgecolor='#002766', zorder=100, label='Dynamic')
 
-plt.legend(['CE','Hur et al.','Enhanced','Dynamic'],fontsize=18,handlelength=1,ncol=4,loc='upper left')
-plt.ylim([0,120])
+plt.legend(['CE', 'Hur et al.', 'Tian et al.', 'Enhanced', 'Dynamic'], fontsize=18, handlelength=1, ncol=1,
+           loc='upper left')
+# plt.ylim([0, 120])
 plt.tight_layout()
-plt.savefig(r'D:\NutStore\BoAndHarry\Paper\2022 JISA\Paper JISA 2022\figures\SUstore.pdf',bbox_inches='tight')
+plt.savefig(r'D:\NutStore\BoAndHarry\Paper\2023 JISA\2nd-submission\paper\figures\Fig. 4-b-SU-upload.pdf', bbox_inches='tight')
 plt.show()
 
-#%%
-plt.figure(3,[8.5,6])
-plt.ylabel('Time cost (ms)',fontsize=25)
-plt.xlabel('File size',fontsize=25)
+# %%
+plt.figure(3, [8.5*factor, 6*factor])
+plt.ylabel('Time cost (ms)', fontsize=25)
+plt.xlabel('File size', fontsize=25)
 plt.xticks(range(length), xLabels)
-plt.grid(zorder = -1, axis='y')
+plt.grid(zorder=-1, axis='y')
 
-barWidth = 0.18
+barWidth = 0.15
 bars1 = retData['mleIU']
 bars2 = retData['hurIU']
-bars3 = retData['enhancedIU']
-bars4 = retData['dynamicIU']
+bars3 = retData['tianIU']
+bars4 = retData['enhancedIU']
+bars5 = retData['dynamicIU']
 
-r1 = np.arange(len(bars1)) - barWidth*1.5
+r1 = np.arange(len(bars1)) - barWidth * 2
 r2 = [x + barWidth for x in r1]
 r3 = [x + barWidth for x in r2]
 r4 = [x + barWidth for x in r3]
+r5 = [x + barWidth for x in r4]
 
-plt.bar(r1, bars1, width=barWidth,color='#003a8c',edgecolor='#002766',zorder=100, label='CE')
-plt.bar(r2, bars2, width=barWidth,color='#096dd9',edgecolor='#002766',zorder=100, label='Hur et al.')
-plt.bar(r3, bars3, width=barWidth,color='#40a9ff',edgecolor='#002766',zorder=100, label='Enhanced')
-plt.bar(r4, bars4, width=barWidth,color='#bae7ff',edgecolor='#002766',zorder=100, label='Dynamic')
+plt.bar(r1, bars1, width=barWidth, color='#001d66', edgecolor='#002766', zorder=100, label='CE')
+plt.bar(r2, bars2, width=barWidth, color='#003eb3', edgecolor='#002766', zorder=100, label='Hur et al.')
+plt.bar(r3, bars3, width=barWidth, color='#1677ff', edgecolor='#002766', zorder=100, label='Tian et al.')
+plt.bar(r4, bars4, width=barWidth, color='#69b1ff', edgecolor='#002766', zorder=100, label='Enhanced')
+plt.bar(r5, bars5, width=barWidth, color='#bae0ff', edgecolor='#002766', zorder=100, label='Dynamic')
 
-plt.legend(['CE','Hur et al.','Enhanced','Dynamic'],fontsize=18,handlelength=1,ncol=4,loc='upper left')
-plt.ylim([0,30])
+plt.legend(['CE', 'Hur et al.', 'Tian et al.', 'Enhanced', 'Dynamic'], fontsize=18, handlelength=1, ncol=1,
+           loc='upper left')
+# plt.ylim([0, 30])
 plt.tight_layout()
-plt.savefig(r'D:\NutStore\BoAndHarry\Paper\2022 JISA\Paper JISA 2022\figures\Retrieve.pdf',bbox_inches='tight')
+plt.savefig(r'D:\NutStore\BoAndHarry\Paper\2023 JISA\2nd-submission\paper\figures\Fig. 4-c-Retrieve.pdf', bbox_inches='tight')
 plt.show()
 
-#%%
+# %%
 # plt.figure(2,[6*1.3,4*1])
 # plt.ylabel('Time cost [ms]')
 # plt.xlabel('File size [MB]')
--- workload/revision.py
+++ workload/revision.py
@@ -9,60 +9,128 @@ from matplotlib import rcParams
 # %%
 class FileGroup:
     users = []
-    alter_consumed = 0
-    add_consumed = 0
+    flag = 1
+
+    def __init__(self, lamb, mue):
+        self.lamb = lamb
+        self.mue = mue
 
     def day_pass(self):
-        new_users = []
+        new_users = self.new_users(np.random.poisson(lam=self.lamb, size=1)[0], self.mue)
+        add_users_count = len(new_users)
+        if len(self.users) == 0 and len(new_users) > 0:
+            initial_upload = 1
+            self.flag = 0
+        else:
+            initial_upload = 0
+
+        self.users += new_users
+        alter_users_count = 0
+        next_users = []
+        current_users = len(self.users)
+        kek_count = 0
         for user in self.users:
             if user >= 1:
                 user -= 1
                 if user == 0:
-                    self.alter_consumed += 1
+                    alter_users_count += 1
+                    kek_count += current_users
+                    current_users -= 1
                 else:
-                    new_users.append(user)
-        res1 = self.alter_consumed
-        res2 = self.add_consumed
-        self.alter_consumed = 0
-        self.add_consumed = 0
-        self.users = new_users
-        return res1, res2
+                    next_users.append(user)
+        self.users = next_users
+        # print(self.users)
+        return alter_users_count, add_users_count, initial_upload, kek_count
 
     def new_users(self, count, mue):
+        new_users = []
         for i in range(count):
-            self.users.append(round(np.random.exponential(scale=mue, size=1)[0]))
-            self.add_consumed += 1
+            new_users.append(round(np.random.exponential(scale=mue, size=1)[0]))
+        return new_users
 
 
 # %%
 
-def simulation(lamb, mue, days, add_rate, alter_rate):
-    file_group = FileGroup()
+fC = 10.0 * 2 ** 20
+fT = 32.0
+fK = 32.
+uid = 32.
+fid = 34.
+r = 16.
+factor = 0.72
+
+def simulation(file_num, lamb, mue, days, initial_cost, subsequent_cost, download_cost, KEK_size):
     cost = []
+    file_group = []
+    for i in range(file_num):
+        file_group.append(FileGroup(lamb, mue))
     for now in range(days):
-        file_group.new_users(np.random.poisson(lam=lamb, size=1)[0], mue)
-        cost_1, cost_2 = file_group.day_pass()
-        cost.append(cost_1 * alter_rate + cost_2 * add_rate)
+        cost.append(0)
+        for i in range(file_num):
+            alter_users_count, add_users_count, initial_upload, kek_count = file_group[i].day_pass()
+            cost[now] += (
+                    alter_users_count * (download_cost + subsequent_cost) +
+                    add_users_count * subsequent_cost +
+                    initial_upload * initial_cost +
+                    kek_count * KEK_size
+            )
 
     return range(days), cost
 
 
 config = {
     "font.family": 'Times New Roman',
-    "font.size": 18,
+    "font.size": 20,
     "mathtext.fontset": 'stix',
     "font.serif": ['Times New Roman'],
 }
 rcParams.update(config)
 
-x1, y1 = simulation(48, 10, 100, 10, 8)
-x2, y2 = simulation(48, 10, 100, 15, 10)
-# x3, y3 = simulation(48, 10, 100, 45, 30)
-plt.figure(1, [8.5, 6])
-plt.plot(x1, y1, 'b')
-plt.plot(x2, y2, 'r')
-plt.legend(["Dynamic", "Enhanced"])
+x1, y1 = simulation(5, 24, 30, 100, fC + fT, fT, fC + fT, 0)
+x2, y2 = simulation(5, 24, 30, 100, fC + fT + fK + uid, fC + fT + fK + uid, fC + fT, fK)
+x3, y3 = simulation(5, 24, 30, 100, fC + fT + fK + uid, fT + r + fK + uid, fC + fT, fK)
+x4, y4 = simulation(5, 24, 30, 100, fT + r + fid, fT + r + fid, 0, fK)
+x5, y5 = simulation(5, 24, 30, 100, fT + r + fid + uid, fT + r + fid + uid, 0, fK)
+
+plt.figure(1, [9*factor, 7*factor])
+plt.plot(x1, y1, 'k-^')
+plt.plot(x2, y2, 'c-s')
+# plt.plot(x3, y3)
+plt.plot(x4, y4, 'r-p')
+plt.plot(x5, y5, 'b-o')
+plt.grid(zorder=-1, axis='y')
+plt.ticklabel_format(style='sci')
+
+plt.legend(["CE", "Hur et al.", "Enhanced", "Dynamic", ],
+# plt.legend(["CE", "Hur et al."],
+           fontsize=18,
+           handlelength=1,
+           ncol=2,
+           loc='best')
 plt.xlabel('Time (days)')
-plt.ylabel('Communication Cost (bytes)')
+plt.ylabel('Communication Cost (in bytes)')
+# plt.xlim([-2, 365])
+# plt.ylim([-5e7, 1e9])
 plt.tight_layout()
+plt.savefig(r'D:\NutStore\BoAndHarry\Paper\2023 JISA\2nd-submission\paper\figures\Bandwidth_all.pdf', bbox_inches='tight')
+plt.show()
+
+
+plt.figure(2, [9*factor, 7*factor])
+plt.plot(x4, y4, 'r-p')
+plt.plot(x5, y5, 'b-o')
+plt.grid(zorder=-1, axis='y')
+plt.ticklabel_format(style='sci')
+plt.legend(["Enhanced", "Dynamic", ],
+# plt.legend(["CE", "Hur et al."],
+           fontsize=18,
+           handlelength=0.8,
+           ncol=1,
+           loc='lower right')
+plt.xlabel('Time (days)')
+plt.ylabel('Communication Cost (in bytes)')
+# plt.xlim([-2, 365])
+# plt.ylim([-100, 7.1e5])
+plt.tight_layout()
+plt.savefig(r'D:\NutStore\BoAndHarry\Paper\2023 JISA\2nd-submission\paper\figures\Bandwidth_ours.pdf', bbox_inches='tight')
 plt.show()
